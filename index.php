5d070faa5ac7fbba3a108fe3056b4c2e650e6341
mylifelineadam <adam@mylifeline.org>
2015-08-12 15:31:34 -0600

    added index2.php and changed index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 	<li>Option 1</li>
 	<li>Option 2</li>
 	<li>Option 3</li>
+	<li>Option 4 NEW</li>
 </ul>
 
 
---
+++ index2.php
@@ -0,0 +1,15 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<ul>
+	<li>Nav 1</li>
+	<li>Nav 2</li>
+	<li>Nav 3</li>
+</ul>
+
+</body>
+</html>
